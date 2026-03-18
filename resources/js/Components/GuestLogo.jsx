5bb928d44bb7b33563f4fe494e2aff1c1d5fb272
Fonguh Joy Akwi <132524733+Akwidiva@users.noreply.github.com>
2026-03-18 11:59:55 +0100

    style: update GuestLogo component with improved image styling and dimensions

--- resources/js/Components/GuestLogo.jsx
+++ resources/js/Components/GuestLogo.jsx
@@ -5,7 +5,12 @@ import logo_CAA from "../Pictures/logo_CAA-big.png"; // Import de l'image PNG
 export default function ApplicationLogo() {
   return (
     <div className="flex align-center">
-      <img src={logo_CAA} alt="Logo de l'application" style={{ width: 250, height: 120 }}/>
+      <img
+        src={logo_CAA}
+        alt="Logo de l'application"
+        className="h-auto w-auto"
+        style={{ maxWidth: 180, height: "auto" }}
+      />
     </div>
   );
 }
